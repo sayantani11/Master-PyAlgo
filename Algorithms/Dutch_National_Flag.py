d6c1a24d3e01356c4455b718c52a431dce2c0e76
sayantani11 <ii.sayantani.ii@gmail.com>
2021-03-14 17:19:49 +0530

    Implemented DNF sort

--- Algorithms/Dutch_National_Flag.py
+++ Algorithms/Dutch_National_Flag.py
@@ -36,8 +36,15 @@ def print_dnf(elements):
 
 if __name__=='__main__':
 
-    numbers=[0,1,1,0,1,2,1,2,0,0,1]
-    size=len(numbers)
+    numbers=[]
 
-    numbers=dnfsort(numbers,size)
+    n=int(input("Enter the number of elements: "))
+
+    print("Enter the elements: ")
+
+    for i in range(0,n):
+        element=int(input())
+
+        numbers.append(element)
+    numbers=dnfsort(numbers,n)
     print_dnf(numbers)
